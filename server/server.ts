e2d7d6aaad7721355d58cdd29267b791eb98427d
gr00stic <dmitriy.kharabara@gmail.com>
2023-04-25 21:48:26 +0300

    Updated cors

--- server/server.ts
+++ server/server.ts
@@ -13,7 +13,10 @@ const PORT = process.env.PORT || 5001;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+}));
 app.use('/api', router);
 app.use(errorHandler);
 
